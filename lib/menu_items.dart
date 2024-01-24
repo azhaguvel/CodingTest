3b48332794b516111dd41c011b71918e2289c3ce
azhaguvel <azhaguvela.89@gmail.com>
2024-01-24 15:28:17 +0530

    Update menu_items.dart
    Uncommented code remove

--- lib/menu_items.dart
+++ lib/menu_items.dart
@@ -1,7 +1,7 @@
 class MenuItem {
   final String name;
   final double price;
-  final int quantity; // Add this property
+  final int quantity; 
 
   MenuItem({required this.name, required this.price, required this.quantity});
 }
